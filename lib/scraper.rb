8e81866b6913691207c21f2c3e0d867fde36e53b
Lena Hou <lenahou2013@gmail.com>
2021-02-23 18:43:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,7 +16,10 @@ class Scraper
         course.description = post.css("p").text
       end
   end
-  binding.pry
+
+  def get_courses
+  self.get_page.css(".post")
+end
 
   def print_courses
       self.make_courses
